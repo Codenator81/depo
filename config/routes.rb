f47fa50a1f1536de621f98068a6164035c17f43a
Oles89 <apoltarjonoks@gmail.com>
2013-07-08 19:27:50 +0300

    Adding a Cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Depot::Application.routes.draw do
+  resources :orders
+
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +13,7 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
